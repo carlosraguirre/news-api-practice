d5c66bf7208b6843f00d71f913f2e36b6ca8b9c3
Carlos Aguirre <craguirre952@gmail.com>
2021-07-07 22:45:01 -0400

    input params and create db

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,8 +1,7 @@
 class NewsController < ApplicationController
 
   def index
-    response = HTTP.get("
-      https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=#{Rails,application.credentials.news_api_key[:api_key]}")
-      render json: response.parse (:json)
+    response = HTTP.get("https://newsapi.org/v2/top-headlines?country=#{params[:country]}&category=business&apiKey=#{Rails.application.credentials.news_api_key[:api_key]}")
+    render json: response.parse(:json)
   end
 end
